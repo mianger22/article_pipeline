aae0fb374e407d1e6f75a3213cf3bf162d2dfe43
BossNumber1 <dimaang23@gmail.com>
2023-11-22 13:22:22 +0300

    started making a page container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ReactNode, useState, useEffect } from "react"
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,15 +14,40 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
+  const navigate = useRouter();
+  const [isAuthed, setAuth] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem('authToken')) {
+      setAuth(true);
+    }
+    // alert(isAuthed);
+  }, [isAuthed])
+
+  const leave_page = () => {
+    // убираем статус авторизованного пользователя
+    setAuth(false);
+
+    // редиректим на страницу авторизации
+    navigate.push('authorization');
+
+    localStorage.removeItem("authToken");
+  }
+  
   return (
     <html lang="ru">
       <body className={inter.className}>
         <header>
           <nav className="flex justify-between items-center px-5 h-[70px] shadow-md bg-gray-50">
             <div>Только свежие новости!</div>
-            <div>Войти</div>
+            {isAuthed && <button 
+              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+              onClick={leave_page}
+            >
+              Выйти
+            </button>}
           </nav>
         </header>
         <main className='px-5 my-5'>
