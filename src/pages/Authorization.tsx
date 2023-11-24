ed57bc81d404a23243736f5f859619ea9d751f17
BossNumber1 <dimaang23@gmail.com>
2023-11-24 16:52:41 +0300

    aligned links right

--- src/pages/Authorization.tsx
+++ src/pages/Authorization.tsx
@@ -23,81 +23,84 @@ export default function Authorization() {
   })
 
   return (
-    <>
-      <Formik
-        initialValues={{ login: '', password: '', repeat_password: '', email: '' }}
-        validate={() => {
-          const errors: ErrorsAuthorizationTypes = {};
+    <Formik
+      initialValues={{ login: '', password: '', repeat_password: '', email: '' }}
+      validate={() => {
+        const errors: ErrorsAuthorizationTypes = {};
 
-          // тут сравнивать values со значениями с сервера: если они не совпадают, выдавать ошибку
+        // тут сравнивать values со значениями с сервера: если они не совпадают, выдавать ошибку
 
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
 
-          localStorage.setItem("authToken", "authToken");
+        localStorage.setItem("authToken", "authToken");
 
-          // редиректим на главную
-          navigate('/');
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <div className="form_custom">
-            <form onSubmit={handleSubmit}>
-              <div className="mb-3">
-                <input
-                  type="text"
-                  name="login"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.login}
-                  placeholder='Псевдоним'
-                  className="input_custom"   
-                />
-                {errors.login && touched.login && errors.login}
-              </div>
-              <div className="mb-3">
-                <input
-                  type="password"
-                  name="password"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                  placeholder='Пароль'
-                  className="input_custom"                
-                />
-                {errors.password && touched.password && errors.password}
-              </div>
+        // редиректим на главную
+        navigate('/');
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <div className="form_custom">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <input
+                type="text"
+                name="login"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.login}
+                placeholder='Псевдоним'
+                className="input_custom"   
+              />
+              {errors.login && touched.login && errors.login}
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                placeholder='Пароль'
+                className="input_custom"                
+              />
+              {errors.password && touched.password && errors.password}
+            </div>
 
-              <div className='flex justify-between items-center mt-5'>
-                <button 
-                  type="submit" 
-                  disabled={isSubmitting} 
-                  className="btn_custom"               
-                >
-                  Войти
-                </button>
+            <div className='flex justify-center items-center mt-5'>
+              <button 
+                type="submit" 
+                disabled={isSubmitting} 
+                className="btn_custom"               
+              >
+                Войти
+              </button>
+            </div>
+          </form>
 
-                <div className='text-blue-700 text-decoration-line: underline text-lg pr-3'>
-                  <Link to="/registration">Зарегистрироваться</Link>
-                </div>
-              </div>
-            </form>
+          <div className='mt-6 px-3 text-blue-700 text-decoration-line: underline text-sm text-right'>
+            <div>
+              <Link to="/password_reset">Вспомнить пароль</Link>
+            </div>
+            <div>
+              <Link to="/registration">Зарегистрироваться</Link>
+            </div>
           </div>
-        )}
-      </Formik>
-    </>
+        </div>
+      )}
+    </Formik>
   );
 }
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -25,108 +25,106 @@ export default function Registration() {
   })
 
   return (
-    <>
-      <Formik
-        initialValues={{ login: '', password: '', repeat_password: '', email: '' }}
-        validate={values=> {
-          const errors: ErrorsRegistrationTypes = {};
+    <Formik
+      initialValues={{ login: '', password: '', repeat_password: '', email: '' }}
+      validate={values=> {
+        const errors: ErrorsRegistrationTypes = {};
 
-          if (!values.email) {
-            errors.email = 'Обязательно для заполнения';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Некорректный email';
-          }
+        if (!values.email) {
+          errors.email = 'Обязательно для заполнения';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Некорректный email';
+        }
 
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
 
-          localStorage.setItem("authToken", "authToken");
+        localStorage.setItem("authToken", "authToken");
 
-          // редиректим на главную
-          navigate('/');
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <div className="form_custom">
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    name="login"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.login}
-                    placeholder='Псевдоним'
-                    className="input_custom"
-                  />
-                  {errors.login && touched.login && errors.login}
+        // редиректим на главную
+        navigate('/');
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <div className="form_custom">
+          <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  name="login"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.login}
+                  placeholder='Псевдоним'
+                  className="input_custom"
+                />
+                {errors.login && touched.login && errors.login}
+              </div>
+              <div className="mb-3">
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  placeholder='Пароль'
+                  className="input_custom"
+                />
+                {errors.password && touched.password && errors.password}
+              </div>
+              <div className="mb-3">
+                <input
+                  type="password"
+                  name="repeat_password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.repeat_password}
+                  placeholder='Повтор пароля'
+                  className="input_custom"
+                />
+                {errors.repeat_password && touched.repeat_password && errors.repeat_password}
+              </div>
+              <div className="mb-3">
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  placeholder='Адрес электронной почты'
+                  className="input_custom"
+                />
+                {errors.email && touched.email && errors.email}
+              </div>
+
+              <div className='flex justify-between items-center mt-5'>
+                <button type="submit" disabled={isSubmitting} className="btn_custom">
+                  Зарегистрироваться
+                </button>
+
+                <div className='text-blue-700 text-decoration-line: underline text-lg pr-3'>
+                  <Link to="/authorization">← Назад</Link>
                 </div>
-                <div className="mb-3">
-                  <input
-                    type="password"
-                    name="password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    placeholder='Пароль'
-                    className="input_custom"
-                  />
-                  {errors.password && touched.password && errors.password}
-                </div>
-                <div className="mb-3">
-                  <input
-                    type="password"
-                    name="repeat_password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.repeat_password}
-                    placeholder='Повтор пароля'
-                    className="input_custom"
-                  />
-                  {errors.repeat_password && touched.repeat_password && errors.repeat_password}
-                </div>
-                <div className="mb-3">
-                  <input
-                    type="email"
-                    name="email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    placeholder='Адрес электронной почты'
-                    className="input_custom"
-                  />
-                  {errors.email && touched.email && errors.email}
-                </div>
-
-                <div className='mt-4'>
-                  <button type="submit" disabled={isSubmitting} className="btn_custom">
-                    Зарегистрироваться
-                  </button>
-                </div>
-            </form>
-          </div>
-        )}
-      </Formik>
-
-      <div className='mt-3'>
-        <Link to="/authorization">← Назад</Link>
-      </div>
-    </>
+              </div>
+          </form>
+        </div>
+      )}
+    </Formik>
   );
 }
