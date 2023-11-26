22c275bdf0676ff2166d9f4fd559887a7bffc1a4
BossNumber1 <dimaang23@gmail.com>
2023-11-26 08:47:11 +0300

    setted height and wight blocks

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 export default function MainPage() {
   return (
-    <>
-      <div>Bio area</div>
-      <div>Current article area or list articles</div>
-      <div>Articles area</div>
-    </>
+    <div className='flex w-full h-full'>
+      <div className='border-2 border-indigo-500/100 w-2/12 flex items-center justify-center'>Bio area</div>
+      <div className='border-2 border-indigo-500/100 w-8/12 flex items-center justify-center'>
+        Current article area or list articles
+      </div>
+      <div className='border-2 border-indigo-500/100 w-2/12 flex items-center justify-center'>Articles area</div>
+    </div>
   );
 }
