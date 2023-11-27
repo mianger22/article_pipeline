c1c542a43eddf97ab7d94db82a77af8d7f43666f
BossNumber1 <dimaang23@gmail.com>
2023-11-27 08:10:21 +0300

    Added block for show list articles

---
+++ src/components/list_articles/ListArticles.tsx
@@ -0,0 +1,5 @@
+export default function ListArticles() {
+  return (
+    <h1>Current article area or list article 2s</h1>
+  );
+}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
+import ListArticles from "../components/list_articles/ListArticles";
+
 export default function MainPage() {
   return (
     <div className='flex w-full h-full'>
       <div className='border-2 border-indigo-500/100 w-2/12 flex items-center justify-center'>Bio area</div>
       <div className='border-2 border-indigo-500/100 w-8/12 flex items-center justify-center'>
-        Current article area or list articles
+        <ListArticles />
       </div>
       <div className='border-2 border-indigo-500/100 w-2/12 flex items-center justify-center'>Articles area</div>
     </div>
