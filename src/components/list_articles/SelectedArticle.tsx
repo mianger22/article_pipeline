a35a3e859a26dfc33489f927fb6a788e6e6f1b56
BossNumber1 <dimaang23@gmail.com>
2023-12-01 13:34:02 +0300

    up

--- src/components/list_articles/SelectedArticle.tsx
+++ src/components/list_articles/SelectedArticle.tsx
@@ -10,14 +10,14 @@ export default function SelectedArticle(props: ListArticlesPropsTypes) {
     <>
       { current_article 
           ?
-            <>
+            <div>
               <Article 
                 title={current_article.title} 
                 link_img={current_article.link_img} 
                 text={current_article.text}
               />
               <ListArticles articles_data={formatted_articles_data} />
-            </>
+            </div>
           : 
             <ListArticles articles_data={props.articles_data} />
       }
